bfa0957325c7303f7ebc2efeef4d32ec208e48ba
xilio <termitetech@163.com>
2025-01-31 11:14:17 +0800

    初始化项目结构

---
+++ src/test/java/cn/xilio/java1024/Java1024ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xilio.java1024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java1024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
